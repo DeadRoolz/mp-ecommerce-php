91d74c5abf1a6f532c787884b5d9a36440cabd95
gean paulo <geanpaulo@MacBook-Air-de-gean.local>
2021-05-30 23:59:27 -0300

    add dbg

---
+++ dbg.php
@@ -0,0 +1,5 @@
+<?php
+
+echo file_get_contents('dbg/mp.json');
+
+die();
--- notification.php
+++ notification.php
@@ -1,7 +1,3 @@
 <?php
 
-    //echo file_get_contents('mp.json');
-
-    //die();
-
     file_put_contents('dbg/mp.json',  file_get_contents("php://input"));
